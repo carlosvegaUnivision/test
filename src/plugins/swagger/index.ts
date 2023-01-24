e7af3344a3ecac67fc73dffa28d2f1b8674100bb
Carlos Vega <v100227@global.ual.com>
2023-01-23 22:06:17 -0500

    perf: improve speed x2

---
+++ src/plugins/swagger/index.ts
@@ -0,0 +1,9 @@
+const swaggerDefinition = {
+  something: {
+    servers: [],
+    info: {
+      description: `Content Search API documentation for installed version: 1.7.1`,
+      version: '1.7.1',
+    }
+  }
+};
